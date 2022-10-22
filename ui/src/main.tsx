b673129dbe0b45d987a58b5876adb5dccc1e5b9b
paradoc <coprada.mj@gmail.com>
2022-10-22 20:40:13 +0900

    added PWA support

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          {/** TODO: Add routes */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
