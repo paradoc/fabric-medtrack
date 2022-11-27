4c11a40013777e3e36f639337703b73f94f5a0ec
paradoc <coprada.mj@gmail.com>
2022-11-27 14:07:10 +0900

    enabled cors

--- gateway/src/internal/handler/getassethandler.go
+++ gateway/src/internal/handler/getassethandler.go
@@ -3,14 +3,18 @@ package handler
 import (
 	"net/http"
 
+	"gateway/src/internal/config"
 	"gateway/src/internal/logic"
 	"gateway/src/internal/svc"
 	"gateway/src/internal/types"
+
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 func GetAssetHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		config.EnableCors(w)
+
 		var req types.ReadRequest
 		if err := httpx.Parse(r, &req); err != nil {
 			httpx.Error(w, err)
