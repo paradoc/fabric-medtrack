747fc3208bb9d1559879089d9381fb267a6269c7
paradoc <coprada.mj@gmail.com>
2022-12-14 18:52:35 +0900

    login improvements; initial commit for dispatcher

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -6,6 +6,15 @@ import './_reset.css'
 import Router from './Router'
 import { store } from './store'
 
+declare global {
+  interface Window {
+    built: string
+  }
+}
+
+// Replaced dynamically by vite.
+window.built = '__DATE__'
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
--- ui/src/views/Dispatcher/Dispatcher.tsx
+++ ui/src/views/Dispatcher/Dispatcher.tsx
@@ -1,9 +1,44 @@
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
 import { Navigate } from 'react-router-dom'
 import { useSessionStorage } from 'usehooks-ts'
 
-export default function Dispatcher() {
-  const [isLoggedIn] = useSessionStorage('login', false)
+import styles from './Dispatcher.module.css'
 
-  return !isLoggedIn ? <Navigate to="/login" /> : <div>dispatcher</div>
+const CurrentTime = () => {
+  const [time, setTime] = useState(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+    }, 1000)
+    return () => {
+      clearInterval(id)
+    }
+  }, [])
+
+  return <div className={styles.time}>{time}</div>
+}
+
+export default function Dispatcher() {
+  const [isLoggedIn, setIsLoggedIn] = useSessionStorage('login', false)
+
+  return !isLoggedIn ? (
+    <Navigate to="/login" />
+  ) : (
+    <div className={styles.container}>
+      <aside>
+        <section className={styles.meta}>
+          <div className={styles.user}>
+            <strong>Mark C.</strong>
+            <span>RPh</span>
+          </div>
+          <CurrentTime />
+        </section>
+        <section className={styles.buttons}>
+          <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+        </section>
+      </aside>
+    </div>
+  )
 }
